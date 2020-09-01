4fa7c360e2e4b8a028ca0cd0b289b1d8efcf4cd2
Steve <34704346+denomous@users.noreply.github.com>
2020-09-01 17:31:20 +0800

    Add files via upload

---
+++ InitPara_Quadruped_walk.m
@@ -0,0 +1,17 @@
+clear;
+
+robot.body.length = 0.9;
+robot.body.width = 0.4;
+robot.body.thickness = 0.15;
+
+robot.body.weight = 5;
+
+robot.hipLegLength = 0;
+robot.upperLegLength = 0.4;
+robot.lowerLegLength = 0.4;
+
+robot.upperLegWeight = 0.2;
+robot.lowerLegWeight = 0.2;
+
+robot.legPosKp = 50;
+robot.legPosKd = 5;
